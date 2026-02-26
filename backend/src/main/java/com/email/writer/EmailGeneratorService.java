3c05879d31d93da5b2cba95740e839526d438750
ArunVasunny <arunvasunny153@gmail.com>
2026-02-27 01:21:41 +0530

    implement email reply generation and prompt

--- backend/src/main/java/com/email/writer/EmailGeneratorService.java
+++ backend/src/main/java/com/email/writer/EmailGeneratorService.java
@@ -4,5 +4,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailGeneratorService {
-    
+
+    public String generateEmailReply(EmailRequest emailRequest) 
+    {    
+        String prompt = buildPrompt(emailRequest);
+        return " ";
+    }
+
+    private String buildPrompt(EmailRequest emailRequest)
+    {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email: ");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty())
+        {
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("Original Email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
 }
+
