b8d3f7fde5bf9312e79b8564b7ea8fd3039ec963
ArunVasunny <arunvasunny153@gmail.com>
2026-03-17 19:05:50 +0530

    implement email reply generation and response extraction

--- backend/src/main/java/com/email/writer/EmailGeneratorService.java
+++ backend/src/main/java/com/email/writer/EmailGeneratorService.java
@@ -1,9 +1,25 @@
 package com.email.writer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class EmailGeneratorService {
+
+    private final WebClient webClient;
+    private final String apiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder, 
+                                @Value("${gemini.api.url}") String baseUrl, 
+                                @Value("${gemini.api.key}") String geminiApiKey)
+    {    
+        this.apiKey = geminiApiKey;
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
+    }
 
     public String generateEmailReply(EmailRequest emailRequest) 
     {    
@@ -22,7 +38,37 @@ public class EmailGeneratorService {
                     ]
                 }""", prompt);
 
-        return " ";
+        String response = webClient.post()
+                        .uri(uriBuilder -> uriBuilder
+                            .path("/v1beta/models/gemini-2.5-flash:generateContent").build())
+                            .header("x-goog-api-key",apiKey)
+                            .header("Content-Type","application/json")
+                            .bodyValue(requestBody)
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .block();
+        
+        return extractResponseContent(response);                    
+    }
+
+    private String extractResponseContent(String response)
+    {
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response);
+            return root.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asString(); 
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest)
