0b6938907d73462b1d1e569c1e9677dd41190b17
ArunVasunny <arunvasunny153@gmail.com>
2026-03-18 19:51:45 +0530

    add loading state and submit button to email reply generator

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,37 @@ import { useState } from 'react'
 import './App.css'
 import FormControl from '@mui/material/FormControl'
 import MenuItem from '@mui/material/MenuItem'
+import Button from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress'
 
 function App() {
   const [emailContent, setEMailContent] = useState('');
-  const [tone, SetTone] = useState('');
+  const [tone, setTone] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+
+  }
 
   return (
     <>
       <Container maxWidth="md" sx={{ mt: 2 }}>
-        <Typography variant='h3' component="h1" gutterBottom>
-          Email Reply Generator
-        </Typography>
+
+      <Typography
+        variant="h3"
+        component="h1"
+        gutterBottom
+        sx={{ textAlign: 'center', fontWeight: 600, mt: 4 }}
+      >
+        Email Reply Generator
+      </Typography>
+
+      <Typography
+        variant="subtitle1"
+        sx={{ textAlign: 'center', color: 'text.secondary', mb: 3 }}
+      >
+        Generate smart replies instantly using AI
+      </Typography>
 
         <Box sx={{mx: 3}}>
           <TextField
@@ -37,7 +57,7 @@ function App() {
             <Select
               value={tone || ''}
               label="Tone (Optional)"
-              onChange={(e) => SetTone(e.target.value)}
+              onChange={(e) => setTone(e.target.value)}
             >
               <MenuItem value="">None</MenuItem>
               <MenuItem value="Professional">Professional</MenuItem>
@@ -45,6 +65,14 @@ function App() {
               <MenuItem value="Casual">Casual</MenuItem>
             </Select>
           </FormControl>
+
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <Button sx={{ mt: 2 }} 
+              variant="contained" 
+              onClick={handleSubmit}
+              disabled={!emailContent || loading}> {loading ? <CircularProgress size={24}/> : "Generate Reply"}
+            </Button>
+          </Box>
         </Box>
       </Container>
     </>
