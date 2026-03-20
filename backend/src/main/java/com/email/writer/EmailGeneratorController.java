9cb6dbc83d841d22ed71cf19187f6e9cfcf62209
ArunVasunny <arunvasunny153@gmail.com>
2026-03-20 23:51:57 +0530

    enable cors

--- backend/src/main/java/com/email/writer/EmailGeneratorController.java
+++ backend/src/main/java/com/email/writer/EmailGeneratorController.java
@@ -1,6 +1,7 @@
 package com.email.writer;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
     
     private final EmailGeneratorService emailGeneratorService;
