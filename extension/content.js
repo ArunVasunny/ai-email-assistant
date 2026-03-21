cf4e5dca507d75d619a2db41768c9561326f6b46
ArunVasunny <arunvasunny153@gmail.com>
2026-03-21 22:15:20 +0530

    implement injectButton and new AI button

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,54 @@
 console.log("Working");
 
+function findComposeToolbar()
+{
+    const selectors = ['.btC','.aDh', '[role="toolbar"]','.gU.Up'];
+    for(const selector of selectors)
+    {
+        const toolbar = document.querySelector(selector);
+        if(toolbar)
+        {
+            return toolbar;
+        }
+        return null;
+    }
+    
+}
+function CreateAIButton()
+{   
+    const button = document.createElement('div');
+    button.className = 'T-I J-J5-Ji aoO v7 T-I-atl L3';
+    button.style.marginRight = '8px';
+    button.innerHTML = 'AI Reply';
+    button.setAttribute('role', 'button');
+    button.setAttribute('data-tooltip', 'Generate AI Reply');
+
+    return button;
+    
+}
+
+function injectButton()
+{
+    const existingButton = document.querySelector('.ai-reply-button')
+    if(existingButton)
+    {
+        existingButton.remove();
+    }
+
+    const toolbar = findComposeToolbar();
+    if(!toolbar)
+    {
+        console.log("Toolbar not found");
+        return;
+    }
+
+    console.log("Toolbar found");
+    const button = CreateAIButton();
+    button.classList.add('ai-reply-button');
+
+    toolbar.insertBefore(button,toolbar.firstChild)
+}
+
 const observer = new MutationObserver((mutations) => {
     for(const mutation of mutations)
     {
@@ -12,6 +61,7 @@ const observer = new MutationObserver((mutations) => {
         if(hasComposeElements)
         {
             console.log("Compose window detected")
+            setTimeout(injectButton,500);
         }
     }
 });
